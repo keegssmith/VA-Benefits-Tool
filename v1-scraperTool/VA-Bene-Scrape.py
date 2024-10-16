ecd80d27343de6a472c124b55de31480c39b1053
pexx <98372449+pex-x@users.noreply.github.com>
2024-10-16 12:02:42 -0600

    Changing to Add COE Elgibility

--- v1-scraperTool/VA-Bene-Scrape.py
+++ v1-scraperTool/VA-Bene-Scrape.py
@@ -2,32 +2,46 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL of the website
-url = "https://dmv.colorado.gov/license-plates-military"
+urlAuto = "https://dmv.colorado.gov/license-plates-military"
+urlHome = "https://www.va.gov/housing-assistance/home-loans/eligibility/"
 
 #User Disability Rating
-userDisRating = 50
+userDisRating = 40
 maleOrFemale = 'm'
+daysOfService = 40
 
 # Make a request to the website
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
+response = requests.get(urlAuto)
+response2 = requests.get(urlHome)
+
+soupAuto = BeautifulSoup(response.content, "html.parser")
+soupHome = BeautifulSoup(response2.content, "html.parser")
+
 
 # Find all paragraphs
-disabled_women_veteran_section = soup.find_all('dt', string="Disabled Women Veteran")
-disabled_veteran_section = soup.find_all('dt', string="Disabled Veteran")
-who_qualifies_sections = soup.find_all('p')
-flag1 = 'temp'
+coElgibility = soupHome.find_all('p')
+who_qualifies_sections = soupAuto.find_all('p')
+flag1 = 'nothing.'
 qualified = False
 
 for section in who_qualifies_sections:
-    if '50% or more Permanent Service-Connected Disabled.' in section.get_text() and maleOrFemale == 'm' and userDisRating >= 50:
+    if 'or more Permanent Service-Connected Disabled.' in section.get_text() and maleOrFemale == 'm' and userDisRating >= 50:
         qualified = True
         flag1 = 'Disabled Veteran License Plate'
-        break
-    elif 'A Woman veteran of the United States Armed Forces who can demonstrate being 50%' in section.get_text() and maleOrFemale == 'f' and userDisRating >= 50:
+        for section in coElgibility:
+            if 'If you\'ve served for at least' and daysOfService >= 90:
+                flag1 =  'Disabled Veteran License Plate and VA Home Loan COE Eligible.' 
+                break
+            else:
+                break
+    elif 'A Woman veteran of the United States Armed Forces who can demonstrate being' in section.get_text() and maleOrFemale == 'f' and userDisRating >= 50:
         qualified = True
-        flag1 = 'Disabled Women Veteran License Plate'
-        break
-
+        flag1 = 'Disabled Woman Veteran License Plate'
+        for section in coElgibility:
+            if 'If you\'ve served for at least' and daysOfService >= 90:
+                flag1 =  'Disabled Veteran License Plate and VA Home Loan COE Eligible.' 
+                break
+            else:
+                break
 if qualified:
     print(f"You Qualify for {flag1}")
