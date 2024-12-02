717038cd4c53550c8040fc658f4b968867535f05
keegssmith <157241021+keegssmith@users.noreply.github.com>
2024-12-02 13:47:01 -0700

    Update website_handle.sql

--- Database/website_handle.sql
+++
@@ -1,3 +0,0 @@
-DECLARE @ColumnName NVARCHAR(MAX) = 'DR_' + CAST(@Rating AS NVARCHAR);
-DECLARE @SQL NVARCHAR(MAX) = 'SELECT ' + @ColumnName + ' FROM States WHERE StateName = @StateName';
-EXEC sp_executesql @SQL, N'@StateName NVARCHAR(MAX)', @StateName;
